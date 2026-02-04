21befa317229bcdd05df1ecf3663810bc40c1d96
sohaibsalmanzc@gmail.com <sohaibsalmanzc@gmail.com>
2026-02-05 05:23:10 +1030

    header component implementation

--- ToDo-Application/src/app/app.ts
+++ ToDo-Application/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Users } from './users/users';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Users, Header],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('ToDo-Application');
---
+++ ToDo-Application/src/app/users/users.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { required } from '@angular/forms/signals';
+
+@Component({
+  selector: 'app-users',
+  imports: [],
+  templateUrl: './users.html',
+  styleUrl: './users.css',
+})
+export class Users {}
